96bc7e49b68b63f513a9a75b524ae62f5360b3d3
Gonéri Le Bouder <goneri@rulezlan.org>
2010-11-18 12:13:14 +0100

    only flag the french files

--- tools/updateStamp.pl
+++ tools/updateStamp.pl
@@ -7,20 +7,48 @@ use POSIX qw(strftime);
 
 use Data::Dumper;
 
-my $file = $ARGV[0];
+my $mainLanguage = "fr";
 
-print $file."\n";
+my $arg = $ARGV[0];
 
-open FD, "<$file" or die;
-my $content;
-{
-    $/=undef;
-    $content = <FD>;
-    my $now_string = strftime "%Y-%m-%e %H:%M:%S", gmtime;
-    $content =~ s/<!--.*-->(\n|)/<!-- LAST_UPDATE=$now_string -->\n/s;
+sub updateFile {
+    my ($file) = @_;
+
+    print "update $file\n";
+
+    open FD, "<$file" or die;
+    my $content;
+    {
+	$/=undef;
+	$content = <FD>;
+	my $now_string = strftime "%Y-%m-%e %H:%M:%S", gmtime;
+	$content =~ s/<!--.*-->(\n|)/<!-- LAST_UPDATE=$now_string -->\n/s;
+    }
+    close FD;
+
+    open FD, ">$file" or die;
+    print FD $content;
+    close FD;
 }
-close FD;
 
-open FD, ">$file" or die;
-print FD $content;
-close FD;
+if ($arg && $arg =~ /^-\d+$/) {
+    foreach (`git log --reverse --name-only $arg 2>&1`) {
+	next if /^commit /;
+	next if /^\S+:\s/;
+	next if /^\s/;
+
+	if (/^(\w{2})\/(\S+.*\S+\.dita)/) {
+	    next unless $mainLanguage eq $1;
+	    updateFile("$1/$2");
+	}
+    }
+} elsif ($arg) {
+    updateFile($arg);
+} else {
+    print STDERR "Flag a file as being the last major version.\n";
+    print STDERR "Usage: $0 [filename | -1]\n";
+    print STDERR "  filename is an DITA XML file\n";
+    print STDERR "  -1 is the number or git revision to flag as major revision. The tool will use git log to get the list of the files.\n";
+}
+
+
